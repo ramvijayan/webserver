ba9d1a4baf82b76c5a76d98aea278f8856a99115
ramanathan <ramanathan.vijayan@gmail.com>
2019-12-05 07:21:13 +0000

    user added module adde

--- httpserver.sh
+++ httpserver.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+#installing httpd package
 yum list installed |grep httpd 
 if [ $? == '0' ]
 then
  echo "package has been installed"
 else
   yum install httpd -y
+fi
+#adding the webdeveloper user
+id webdeveloper  >/dev/null 2>&1
+if [ $? -eq '0' ]; then 
+ echo "user has been created alreday"
+else
+ adduser webdeveloper >/dev/null 2>&1
 fi
